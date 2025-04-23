872ed4478708bad8ee3cbaaa7be4637f52883844
Mofeleng <mofelengmokgope@gmail.com>
2025-04-23 22:45:31 +0200

    Finished desktop

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar() {
             <div className="flex justify-between items-center h-24">
                 <div className="text-xl font-semibold">Mofeleng Mokgope</div>
                 <ul className="flex space-x-8">
-                    <li><a href="/" className="hover:font-bold">Work</a></li>
-                    <li><a href="./#about" className="hover:font-bold">About</a></li>
-                    <li><a href="#resume" className="hover:font-bold bg-gradient-to-r from-blue-400 to-blue-800 rounded-sm p-3 text-white">Resume</a></li>
+                    <li><a href="./#projects" className="hover:font-bold">Work</a></li>
+                    <li><a href="https://github.com/Mofeleng" className="hover:font-bold" target='blank'>Github</a></li>
+                    <li><a href="#resume" className="hover:font-bold bg-gradient-to-r from-blue-400 to-blue-800 bg-clip-text text-transparent rounded-sm p-3">Resume</a></li>
                 </ul>
             </div>
         </div>
