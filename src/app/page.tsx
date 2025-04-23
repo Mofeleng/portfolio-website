0f557380ba71c8593f8b3dea9339ccc91450f6e7
Mofeleng <mofelengmokgope@gmail.com>
2025-04-23 22:56:18 +0200

    Made site responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,23 @@ import { heading } from "@/lib/constants/fonts";
 export default function Home() {
   return (
     <main>
-      <section className="flex flex-col items-center justify-center min-h-[80vh]">
+      <section className="flex flex-col py-16 md:py-0 items-center justify-center min-h-[80vh]">
           <div className="max-w-5xl w-full px-8">
               <div className="grid md:grid-cols-2 gap-10">
                 <div className="space-y-6 flex flex-col justify-center">
-                  <h2 className={cn("text-3xl font-bold", heading.className)}>Hi, I'm Mofeleng,
+                  <h2 className={cn("text-2xl md:text-3xl font-bold", heading.className)}>Hi, I'm Mofeleng,
                     a <ColoredText>full stack developer</ColoredText> and <ColoredText>data analyst</ColoredText> with a passion for business and technology.</h2>
                   <ul className="space-y-3">
                       <li>
                         <span className="text-gray-500">Maths and Computer Science Student</span>
                       </li>
-                      <li className="flex flex-row gap-2 items-center">
-                        <span className="text-gray-500">Previously Freelanced at </span>
+                      <li className="">
+                        <span className="text-gray-500 inline-block mr-2">Previously Freelanced at </span>
                         <Link href='https://parawild.netlify.app'
                               target="blank"
-                              className="flex flex-row items-center hover:underline"
+                              className="hover:underline"
                         >
-                          <LinkIcon width={12} height={12}/>
+                          <LinkIcon className="w-3 h-3 inline-block mr-1"/>
                           Parawild Edu Capture
                         </Link>
                       </li>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,74 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className='h-24 flex flex-col justify-center items-center'>
-        <div className="container mx-auto px-8">
-            <div className="flex justify-between items-center h-24">
-                <div className="text-xl font-semibold">Mofeleng Mokgope</div>
-                <ul className="flex space-x-8">
-                    <li><a href="./#projects" className="hover:font-bold">Work</a></li>
-                    <li><a href="https://github.com/Mofeleng" className="hover:font-bold" target='blank'>Github</a></li>
-                    <li><a href="#resume" className="hover:font-bold bg-gradient-to-r from-blue-400 to-blue-800 bg-clip-text text-transparent rounded-sm p-3">Resume</a></li>
-                </ul>
-            </div>
+    <nav className="h-24 flex flex-col justify-center items-center">
+      <div className="container mx-auto px-8">
+        <div className="flex justify-between items-center h-24">
+          <div className="text-xl font-semibold">Mofeleng Mokgope</div>
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-gray-800 focus:outline-none hover:cursor-pointer"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+          <ul
+            className={`md:flex space-x-8 ${
+              isOpen ? 'block' : 'hidden'
+            } absolute md:static top-24 left-0 w-full bg-white md:bg-transparent md:w-auto md:space-y-0 space-y-4 p-4 md:p-0`}
+          >
+            <li>
+              <a href="./#projects" className="hover:font-bold">
+                Work
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Mofeleng"
+                className="hover:font-bold"
+                target="blank"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="#resume"
+                className="hover:font-bold bg-gradient-to-r from-blue-400 to-blue-800 bg-clip-text text-transparent rounded-sm"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-  )
+  );
 }
